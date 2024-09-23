4bdc83147b785a83fd7f8b921139e2487d010eec
uncommonlibrary <carmen916@gmail.com>
2024-09-23 15:05:38 +0800

    chore: test verifyToken

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
+
 const {
   createUser,
   logInUser,
   getAllUsers,
   getUserByUsername,
 } = require("../controllers/userController");
+
+const { verifyToken } = require("../middleware/tokenMiddleware");
 
 const router = express.Router();
 
@@ -16,6 +19,9 @@ router.post("/login", logInUser);
 
 // get all users
 router.get("/all", getAllUsers);
+
+// testing token
+router.get("/test", verifyToken, (req, res) => res.json("test"));
 
 // get user by username
 router.get("/:username", getUserByUsername);
