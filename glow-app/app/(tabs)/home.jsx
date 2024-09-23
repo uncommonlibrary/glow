2d439e095d31aa2779d1bd5344c8f2c5ee51aa12
uncommonlibrary <carmen916@gmail.com>
2024-09-23 15:08:14 +0800

    feat: homepage

--- glow-app/app/(tabs)/home.jsx
+++ glow-app/app/(tabs)/home.jsx
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, FlatList } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "../../components/EmptyState";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-background h-full">
+      <Text
+        className="text-primary text-center"
+        style={{ fontFamily: "Pacifico-Regular", fontSize: "30" }}
+      >
+        Glow
+      </Text>
+      <FlatList
+        data={[]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text className="text-3xl">{item.id}</Text>}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="Nothing here!"
+            subtitle="Start following people to get started."
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
