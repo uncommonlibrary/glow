fcbd400cddaf1635d5e0466198ea27855055baf9
uncommonlibrary <carmen916@gmail.com>
2024-09-23 13:44:49 +0800

    feat: frontend setup

---
+++ glow-app/app/_layout.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Slot } from "expo-router";
+
+const RootLayout = () => {
+  return <Slot />;
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
