6ef5a52a4fe3c3212a6a301132bb64b79cd0e181
uncommonlibrary <carmen916@gmail.com>
2024-09-23 22:31:16 +0800

    feat: post model

---
+++ backend/models/Post.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    textContent: { type: String, required: true },
+    postPhoto: { type: String, required: true },
+    makeupProduct: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
